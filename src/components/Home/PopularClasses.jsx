9e4ca8b395345952384e8cfd98208d6cc943f230
DevvSakib <devvsakib@gmail.com>
2023-06-12 19:57:49 +0600

    darkmode completed

--- src/components/Home/PopularClasses.jsx
+++ src/components/Home/PopularClasses.jsx
@@ -15,9 +15,37 @@ const PopularClasses = () => {
         <div className="my-32">
             <LayoutSize>
                 <MFTitle title="Popular Classes" tagline="Discover our popular classes and explore a diverse range of disciplines." />
-                {
-                    isLoading && <p>Loading...</p>
-                }
+                {isLoading && <div className='mt-5 flex flex-col items-center text-center'>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 100 100"
+            width={80}
+            preserveAspectRatio="xMidYMid"
+            style={{ background: 'none' }}
+          >
+            <circle
+              cx="50"
+              cy="50"
+              fill="none"
+              stroke="#EC3899"
+              strokeWidth="10"
+              r="30"
+              strokeDasharray="165 57"
+            >
+              <animateTransform
+                attributeName="transform"
+                type="rotate"
+                repeatCount="indefinite"
+                dur="1s"
+                values="0 50 50;360 50 50"
+                keyTimes="0;1"
+              ></animateTransform>
+            </circle>
+          </svg>
+          <p>
+            Loading...
+          </p>
+        </div>}
                 {
                     classes?.data.length === 0 && <p className="text-center">Be the first Publisher?</p>
                 }
--- src/components/Home/PopularInstructors.jsx
+++ src/components/Home/PopularInstructors.jsx
@@ -32,7 +32,37 @@ const PopularInstructors = () => {
         <MFTitle title="Popular Instructors" tagline="Our popular instructors and explore a diverse range of disciplines." />
         <div className="grid md:grid-cols-2 xl:grid-cols-3 gap-y-7 md:gap-10 pt-5">
           {
-            loading ? <div className="text-center">Loading...</div> : error ? <div className="text-center">Error</div> : instructors.length ?
+            loading ? <div className='mt-5 flex flex-col items-center text-center'>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 100 100"
+                width={80}
+                preserveAspectRatio="xMidYMid"
+                style={{ background: 'none' }}
+              >
+                <circle
+                  cx="50"
+                  cy="50"
+                  fill="none"
+                  stroke="#EC3899"
+                  strokeWidth="10"
+                  r="30"
+                  strokeDasharray="165 57"
+                >
+                  <animateTransform
+                    attributeName="transform"
+                    type="rotate"
+                    repeatCount="indefinite"
+                    dur="1s"
+                    values="0 50 50;360 50 50"
+                    keyTimes="0;1"
+                  ></animateTransform>
+                </circle>
+              </svg>
+              <p>
+                Loading...
+              </p>
+            </div> : error ? <div className="text-center">Error</div> : instructors.length ?
               instructors.map((instructor, ind) => (
                 <InstructorCard key={ind} instructor={instructor} />
               ))
@@ -40,10 +70,10 @@ const PopularInstructors = () => {
               <div className="text-center col-span-8">No Instructors</div>
           }
         </div>
-      {!instructors.length == 0 && 
-      <div className="text-center mt-10">
-          <MFButton text="All Instructors" path="/instructors" />
-        </div>
+        {!instructors.length == 0 &&
+          <div className="text-center mt-10">
+            <MFButton text="All Instructors" path="/instructors" />
+          </div>
         }
       </LayoutSize>
     </div >
